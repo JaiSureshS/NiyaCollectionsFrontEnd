871976eb5268aa0eb409ce4232ea31fb78901858
JaiSureshS <krishnareddy.saragada12@gmail.com>
2022-11-14 22:16:39 -0500

    fix

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,15 +1,19 @@
 const path = require('path');
-    
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        static: path.resolve(__dirname, 'dist'),
-        compress: true,
-        port: 8080,
-    },
-    mode: 'production'
+  entry: {
+	app: './src/index.js',
+  },
+  plugins: [
+	new HtmlWebpackPlugin({
+	  title: 'Production',
+	}),
+  ],
+  output: {
+	filename: 'main.js',
+	path: path.resolve(__dirname, 'dist'),
+	clean: true,
+  },
+  mode: 'production'
 };
