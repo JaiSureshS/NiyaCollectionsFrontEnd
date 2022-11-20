645fe812d1da4b151530ff85d188aca4b4b0303a
JaiSureshS <krishnareddy.saragada12@gmail.com>
2022-11-19 23:37:46 -0500

    init

--- backend/config.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-export default {
-	PORT: process.env.PORT || 5002,
-	MONGODB_URL: process.env.MONGODB_URL,
-	JWT_SECRET: process.env.JWT_SECRET,
-};
